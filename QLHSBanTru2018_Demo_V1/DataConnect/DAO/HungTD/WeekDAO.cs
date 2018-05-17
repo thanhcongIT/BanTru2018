9ef07717142db01d7333a098b75d40e71e28752a
duchung1510 <duchung.1510@gmail.com>
2018-05-17 17:25:04 +0700

    Vớ vẩn.

---
+++ QLHSBanTru2018_Demo_V1/DataConnect/DAO/HungTD/WeekDAO.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataConnect.DAO.HungTD
+{
+    public class WeekDAO
+    {
+        QLHSSmartKidsDataContext db;
+        Table<Week> weeks;
+        public WeekDAO()
+        {
+            db = new QLHSSmartKidsDataContext();
+            weeks = db.GetTable<Week>();
+        }
+        public List<Week> ListAll(int courseID)
+        {
+            try
+            {
+                var model = weeks.Where(x => x.CourseID.Equals(courseID));
+                return model.ToList();
+            }
+            catch
+            {
+                return null;
+            }
+        }
+        public int Insert(Week entity)
+        {
+            try
+            {
+                weeks.InsertOnSubmit(entity);
+                db.SubmitChanges();
+                return entity.WeekID;
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+        public bool InsertAllWeekOfCourse(Course course)
+        {
+            try
+            {
+                int iWeekIndex = 1;
+                for (DateTime date = course.StartDate; date.Date <= course.EndDate; date = date.AddDays(1))
+                {
+                    if (date.DayOfWeek == DayOfWeek.Monday)
+                    {
+                        Week week = new Week();
+                        week.CourseID = course.CourseID;
+                        week.WeekIndex = iWeekIndex;
+                        week.StartDate = date;
+                        week.EndDate = date.AddDays(6);
+                        week.Status = true;
+                        iWeekIndex += 1;
+
+                        if (Insert(week) <= 0)
+                        {
+                            break;
+                        }
+                    }
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
