a0dc9a9cf033aec3b5bbe7387c8a9939cde14378
duchung1510 <duchung.1510@gmail.com>
2018-05-18 13:40:12 +0700

    A
    TDH

---
+++ QLHSBanTru2018_Demo_V1/DataConnect/ViewModel/DailyTrackerFullViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataConnect.ViewModel
+{
+    public class DailyTrackerFullViewModel
+    {
+        public int DailyTrackerID { get; set; }
+        public int StudentID { get; set; }
+        public string StudentFirstName { get; set; }
+        public string StudentLastName { get; set; }
+        public string StudentHomeName { get; set; }
+        public int WeekID { get; set; }
+        public DateTime Date { get; set; }
+        public int Present { get; set; }
+        public string PresentString { get; set; }
+        public string Reason { get; set; }
+        public TimeSpan? TimeIn { get; set; }
+        public TimeSpan? TimeOut { get; set; }
+        public string DrugTime { get; set; }
+        public int? Eating { get; set; }
+        public int? Sleep { get; set; }
+        public int? Health { get; set; }
+        public int? Study { get; set; }
+        public string Note { get; set; }
+        public bool Status { get; set; }
+
+        public bool Monday { get; set; }
+        public bool Tuesday { get; set; }
+        public bool Wednesday { get; set; }
+        public bool Thursday { get; set; }
+        public bool Friday { get; set; }
+        public bool Saturday { get; set; }
+        public bool Sunday { get; set; }
+    }
+}
