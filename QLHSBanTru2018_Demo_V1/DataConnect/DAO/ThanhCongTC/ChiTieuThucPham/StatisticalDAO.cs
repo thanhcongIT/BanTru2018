cef094782620e1e18a466edc18884e1478fb6734
NGUYEN KIEU THANH CONG <1521050336@student.humg.edu.vn>
2018-05-19 17:44:42 +0700

    changes

--- QLHSBanTru2018_Demo_V1/DataConnect/DAO/ThanhCongTC/ChiTieuThucPham/StatisticalDAO.cs
+++ QLHSBanTru2018_Demo_V1/DataConnect/DAO/ThanhCongTC/ChiTieuThucPham/StatisticalDAO.cs
@@ -10,32 +10,33 @@ namespace DataConnect.DAO.ThanhCongTC.ChiTieuThucPham
     public class StatisticalDAO
     {
         QLHSSmartKidsDataContext dt = new QLHSSmartKidsDataContext();
-        public List<OrderOrderDetailViewModle> ListOrderOrderDetailViewModelByDay(int IngredientID,DateTime dateTime)
+        public List<OrderOrderDetailViewModle> ListOrderOrderDetailViewModelByDay(int? IngredientID,DateTime dateTime)
         {
             var a = dt.Orders.Where(t => t.Date == dateTime);
             a.ToList();
             List<OrderOrderDetailViewModle> b = new List<OrderOrderDetailViewModle>();
-            foreach (var i in a)
-            {
-                OrderDetail c = dt.OrderDetails.FirstOrDefault(t => t.IngredientID == IngredientID && t.OrderID == i.OrderID);
-                OrderOrderDetailViewModle c1 = new OrderOrderDetailViewModle();
-                if (c!=null)
+            
+                foreach (var i in a)
                 {
-                    c1.OrderID = i.OrderID;
-                    c1.OrderName = i.OrderName;
-                    c1.Date = i.Date;
-                    c1.EmployeeID = i.EmployeeID;
-                    c1.IngredientID = c.IngredientID;
-                    c1.Name = c.Ingredient.Name;
-                    c1.PriceOfUnit = c.PriceOfUnit;
-                    c1.Unit = c.Ingredient.Unit;
-                    c1.QuantityOfUnit = c.QuantityOfUnit;
-                    c1.TotalPrice = c.TotalPrice;
-                    b.Add(c1);
+                    OrderDetail c = dt.OrderDetails.FirstOrDefault(t => t.IngredientID == IngredientID && t.OrderID == i.OrderID);
+                    OrderOrderDetailViewModle c1 = new OrderOrderDetailViewModle();
+                    if (c != null)
+                    {
+                        c1.OrderID = i.OrderID;
+                        c1.OrderName = i.OrderName;
+                        c1.Date = i.Date;
+                        c1.EmployeeID = i.EmployeeID;
+                        c1.IngredientID = c.IngredientID;
+                        c1.Name = c.Ingredient.Name;
+                        c1.PriceOfUnit = c.PriceOfUnit;
+                        c1.Unit = c.Ingredient.Unit;
+                        c1.QuantityOfUnit = c.QuantityOfUnit;
+                        c1.TotalPrice = c.TotalPrice;
+                        b.Add(c1);
+                    }
+
                 }
-                
-            }
-            return b;
+                return b;          
         }
     }
 }
--- QLHSBanTru2018_Demo_V1/QLHSBanTru2018_Demo_V1/QLThuChi/ChiTieu/ChiTieuThucPham/ThongKeThucPham/UsThongKeThucPham.cs
+++ QLHSBanTru2018_Demo_V1/QLHSBanTru2018_Demo_V1/QLThuChi/ChiTieu/ChiTieuThucPham/ThongKeThucPham/UsThongKeThucPham.cs
@@ -24,38 +24,87 @@ namespace QLHSBanTru2018_Demo_V1.QLThuChi.ChiTieu.ChiTieuThucPham.ThongKeThucPha
             IngredientTypeDAO dt = new IngredientTypeDAO();
             grLoaiThucPham.DataSource = dt.ListAll();
         }
+        public void loadLoaiTP()
+        {
+            IngredientTypeDAO dt = new IngredientTypeDAO();
+            cbbLoaithucPham.DataSource = dt.ListAll();
+            cbbLoaithucPham.ValueMember = "IngredientTypeID";
+            cbbLoaithucPham.DisplayMember = "Name";
+        }
         public void LoadDanhSachThucPham(int IngredientTypeID)
         {
             IngredientDAO dt = new IngredientDAO();
-            grDanhSachThucPham.DataSource = dt.ListAllActive(IngredientTypeID);
+            grLoaiThucPham.DataSource = dt.ListAllActive(IngredientTypeID);
         }
         public void LoadThongKeThucPham(int IngredientID,DateTime Date)
         {
             StatisticalDAO dt = new StatisticalDAO();
             grThongKeThucPham.DataSource = dt.ListOrderOrderDetailViewModelByDay(IngredientID, Date);
         }
+        public void TinhTong()
+        {
+            decimal TongTien = 0;
+            double TongSoLuong = 0;
+            for (int i = 0; i < gridView3.RowCount; i++)
+            {
+                TongTien += (decimal)gridView3.GetRowCellValue(i, "TotalPrice");
+                TongSoLuong += (double)gridView3.GetRowCellValue(i, "QuantityOfUnit");
+            }
+            txtSoLuong.Text = TongSoLuong.ToString();
+            txtTongTien.Text = TongTien.ToString();
+        }
         private void UsThongKeThucPham_Load(object sender, EventArgs e)
         {
-            LoadLoaiThucPham();
-            LoadDanhSachThucPham((int)gridView1.GetRowCellValue(gridView1.FocusedRowHandle, "IngredientTypeID"));
-            LoadThongKeThucPham((int)gridView2.GetRowCellValue(gridView2.FocusedRowHandle, "IngredientID"), dtTheoNgay.Value);
+            loadLoaiTP();
+            cbbLoaithucPham_SelectedIndexChanged(sender, e);
+            LoadDanhSachThucPham((int)cbbLoaithucPham.SelectedValue);
         }
 
         private void gridView1_FocusedRowChanged(object sender, DevExpress.XtraGrid.Views.Base.FocusedRowChangedEventArgs e)
         {
-            LoadDanhSachThucPham((int)gridView1.GetRowCellValue(e.FocusedRowHandle, "IngredientTypeID"));
+            LoadThongKeThucPham((int)gridView1.GetRowCellValue(e.FocusedRowHandle, "IngredientID"), dtTheoNgay.Value);
+            TinhTong();
         }
-
-        private void gridView2_FocusedRowChanged(object sender, DevExpress.XtraGrid.Views.Base.FocusedRowChangedEventArgs e)
-        {
-            groupControl2.Text = gridView2.GetRowCellValue(e.FocusedRowHandle, "Name").ToString();
-            LoadThongKeThucPham((int)gridView2.GetRowCellValue(gridView2.FocusedRowHandle, "IngredientID"), dtTheoNgay.Value);
-            MessageBox.Show("" + (int)gridView2.GetRowCellValue(gridView2.FocusedRowHandle, "IngredientID") + "");
-        }
-
         private void dtTheoNgay_ValueChanged(object sender, EventArgs e)
         {
-            LoadThongKeThucPham((int)gridView2.GetRowCellValue(gridView2.FocusedRowHandle, "IngredientID"), dtTheoNgay.Value);
+            LoadThongKeThucPham((int)gridView1.GetRowCellValue(gridView1.FocusedRowHandle, "IngredientID"), dtTheoNgay.Value);
+            TinhTong();           
+        }
+
+        private void gridView3_FocusedRowChanged(object sender, DevExpress.XtraGrid.Views.Base.FocusedRowChangedEventArgs e)
+        {
+            //MessageBox.Show("" + gridView3.GetRowCellValue(e.FocusedRowHandle, "QuantityOfUnit") + "");
+        }
+
+        private void cbTheoNgay_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cbTheoThang_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cbTheoNam_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cbbLoaithucPham_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                int a = (int)cbbLoaithucPham.SelectedValue;
+                LoadDanhSachThucPham(a);
+                LoadThongKeThucPham((int)gridView1.GetRowCellValue(gridView1.FocusedRowHandle, "IngredientID"), dtTheoNgay.Value);
+                TinhTong();
+            }
+            catch 
+            {
+
+                
+            }
         }
     }
 }
